77da228344d319a25078f1a288c7c0178772edc1
qxbthree <qxbthree@gmail.com>
2024-12-24 18:52:26 +0800

    ags: notif_popups - make each notif popup unique

--- .config/ags/windows/notification_popups/NotificationPopups.tsx
+++ .config/ags/windows/notification_popups/NotificationPopups.tsx
@@ -1,5 +1,6 @@
 import Notifyd from 'gi://AstalNotifd'
 import { Astal, Gdk, Gtk } from 'astal/gtk3'
+import { GLib } from 'astal'
 
 import { Notification } from '@widgets'
 import { revealNotificationCenter } from '@windows/notification_center/vars'
@@ -8,7 +9,7 @@ import { revealCalendar } from '@windows/calendar/vars'
 const notifyd = Notifyd.get_default()
 
 function NotifPopupList() {
-  const popups = new Map<number, Gtk.Widget>()
+  const popups = new Map<string, Gtk.Widget>()
 
   return (
     <box
@@ -22,22 +23,24 @@ function NotifPopupList() {
           if (!notification || notifyd.get_dont_disturb())
             return
 
+          const uniqueID = GLib.uuid_string_random()
+
           const notificationWidget = Notification({
             notification,
             popup: true,
-            onClick: (remove) => onRemove(id, remove),
-            onHoverLost: (remove) => onRemove(id, remove),
-            onPopupTimeoutDone: (remove) => onRemove(id, remove)
+            onClick: (remove) => onRemove(uniqueID, remove),
+            onHoverLost: (remove) => onRemove(uniqueID, remove),
+            onPopupTimeoutDone: (remove) => onRemove(uniqueID, remove)
           })
 
           revealNotificationCenter.set(false)
           revealCalendar.set(false)
 
-          popups.set(id, notificationWidget)
+          popups.set(uniqueID, notificationWidget)
           self.pack_start(notificationWidget, false, false, 0)
         }
 
-        function onRemove(id: number, remove: () => void) {
+        function onRemove(id: string, remove: () => void) {
           if (!popups.has(id))
             return
 
