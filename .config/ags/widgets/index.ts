8314d6018d1a657ac05c53c53a3e3b0bec36cf35
qxbthree <qxbthree@gmail.com>
2024-12-28 20:49:13 +0800

    ags: really buggy network management (connect/disconnect)

--- .config/ags/widgets/index.ts
+++ .config/ags/widgets/index.ts
@@ -4,3 +4,4 @@ export { default as Notification } from './notification/Notification'
 
 export { default as FlowBox } from './FlowBox'
 export { default as ProgressBar } from './ProgressBar'
+export { default as CheckButton } from './CheckBox'
