89be1165e0c511db5da6f65d91a22f0d16fbbe66
qxbthree <qxbthree@gmail.com>
2025-02-06 04:39:57 +0800

    update wallpaper daemon

--- .config/hypr/scripts/wallpaperdaemon.sh
+++ .config/hypr/scripts/wallpaperdaemon.sh
@@ -2,5 +2,13 @@
 
 swww query
 if [ $? -eq 1 ]; then
+  random_x=$(echo "scale=2; $RANDOM/32767" | bc)
+  random_y=$(echo "scale=2; $RANDOM/32767" | bc)
+
   swww-daemon --format xrgb &
+
+  swww img ~/.local/state/retro/current_wallpaper \
+    --transition-type "grow" \
+    --transition-pos $random_x,$random_y \
+    --transition-duration 3
 fi
